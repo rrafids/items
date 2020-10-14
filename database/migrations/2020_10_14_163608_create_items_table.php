20eb2d16cb131a7f1ecb62c1b97c8bf6dfd90a71
rrafids <55825072+rrafids@users.noreply.github.com>
2020-10-15 00:41:09 +0700

    Update 2020_10_14_163608_create_items_table.php

--- database/migrations/2020_10_14_163608_create_items_table.php
+++ database/migrations/2020_10_14_163608_create_items_table.php
@@ -14,7 +14,7 @@ class CreateItemsTable extends Migration
     public function up()
     {
         Schema::create('items', function (Blueprint $table) {
-            $table->string('code');
+            $table->string('item_code');
             $table->string('name');
             $table->longText('description');
         });
